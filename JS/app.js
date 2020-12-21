f209a9e16eb4774a6bf8b39882c57473d5359a2e
Kim Long <kimrlong09@gmail.com>
2020-12-21 18:30:36 -0500

    connected Zodiac API to live server

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,26 @@
-<p>hello, adding to rep...again</p>
+
+
+let opts = {
+	mystic_dob: "1987-05-21",
+	mystic_name: "John"
+}
+fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
+	{
+		method: 'post',
+		headers: {
+			"content-type": "application/x-www-form-urlencoded",
+			"x-rapidapi-key": "5ebc03e641msh91de1db69ede71dp13e0f0jsnb8c37847f3b1",
+			"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com",
+			"useQueryString": true
+		},
+		body: JSON.stringify(opts)
+	})
+	.then(result => result.json())
+	.then(data => {
+		console.log(data)
+	})
+
+
+
+
 
