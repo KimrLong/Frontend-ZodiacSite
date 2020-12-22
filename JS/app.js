2bd94d1aab5724f58564b5826710c2020b2761d0
Kim Long <kimrlong09@gmail.com>
2020-12-22 10:31:32 -0500

    got a working zodiac API

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,12 @@
 
 
 let opts = {
-	mystic_dob: "1987-05-21",
+	mystic_dob: "1986-08-30",
 	mystic_name: "John"
+	
 }
+
+let travelInfo = 
 fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
 	{
 		method: 'post',
@@ -15,12 +18,36 @@ fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
 		},
 		body: JSON.stringify(opts)
 	})
-	.then(result => result.json())
-	.then(data => {
+	travelInfo.then((results) => {
+		console.log(results.json())
+
+	})
+		
+
+	.then((data) => {
 		console.log(data)
 	})
 
 
 
 
+
+
+
+
+// fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
+// 	{
+// 		method: 'post',
+// 		headers: {
+// 			"content-type": "application/x-www-form-urlencoded",
+// 			"x-rapidapi-key": "5ebc03e641msh91de1db69ede71dp13e0f0jsnb8c37847f3b1",
+// 			"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com",
+// 			"useQueryString": true
+// 		},
+// 		body: JSON.stringify(opts)
+// 	})
+// 	.then(result => result.json())
+// 	.then(data => {
+// 		console.log(data)
+// 	})
 
