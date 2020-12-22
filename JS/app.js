4996abf1068595e86af2754bf668f8185ad1d70f
Kim Long <kimrlong09@gmail.com>
2020-12-22 10:55:45 -0500

    merging changes

--- JS/app.js
+++ JS/app.js
@@ -22,3 +22,24 @@ fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
 		console.log(data)
 	})
 
+	// let travelInfo = 
+	// fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
+	// 	{
+	// 		method: 'post',
+	// 		headers: {
+	// 			"content-type": "application/x-www-form-urlencoded",
+	// 			"x-rapidapi-key": "5ebc03e641msh91de1db69ede71dp13e0f0jsnb8c37847f3b1",
+	// 			"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com",
+	// 			"useQueryString": true
+	// 		},
+	// 		body: JSON.stringify(opts)
+	// 	})
+	// 	travelInfo.then((results) => {
+	// 		console.log(results.json())
+	
+	// 	})
+			
+	
+	// 	.then((data) => {
+	// 		console.log(data)
+	// 	})
