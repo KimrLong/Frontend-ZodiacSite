253d871abd9116929836a5d8279097c7c72966d6
Matthew <Matthewtroberts10@gmail.com>
2020-12-23 09:47:47 -0600

    Updated information about the travel API and additional notes

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,18 @@
-
-
-let opts = {
-	mystic_dob: "1986-08-30",
-	mystic_name: "John"
-	
-}
-
-fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
-	{
-		method: 'post',
-		headers: {
-			"content-type": "application/x-www-form-urlencoded",
-			"x-rapidapi-key": "5ebc03e641msh91de1db69ede71dp13e0f0jsnb8c37847f3b1",
-			"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com",
-			"useQueryString": true
-		},
-		body: JSON.stringify(opts)
-	})
-	.then(result => result.json())
-	.then(data => {
-		console.log(data)
-	})
-
+fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"x-rapidapi-key": "5ebc03e641msh91de1db69ede71dp13e0f0jsnb8c37847f3b1",
+		"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com"
+	},
+	"body": {
+		"mystic_dob": "1987-05-21",
+		"mystic_name": "John"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
--- JS/travelAPI.js
+++ JS/travelAPI.js
@@ -1,1 +1,3 @@
-https://www.triposo.com/api/20201111/location.json?id=London&account=<account ID>&token=<API token>
+// https://www.triposo.com/api/20201111/location.json?id=London&account=<account ID>&token=<API token>
+
+
