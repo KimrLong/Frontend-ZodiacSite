6d8a53bb8c81d34900c7f9be8dd9994a91341539
Kim Long <kimrlong09@gmail.com>
2020-12-30 16:05:51 -0500

    burger toggle & JS for modals

--- JS/app.js
+++ JS/app.js
@@ -21,40 +21,30 @@ fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/", {
 
 
 
-// function newFunction (month){
-// 		let opts = {
-// 		mystic_dob: month, //"1987-10-17",
-// 		mystic_name: "Kanny",
-	
-// } //make a variable where it says date of birth//initialize
-// }
-// let tavelInfo =
-
-
 
 
 var Cap = "2000-01-17"
 var Aqu = "2000-02-17"
 var Pisces = "2000-03-17"
-let Aries = "2000-04-17"
+var Aries = "2000-04-17"
 var Taurus = "2000-05-17"
 var Gemini = "2000-06-17"
 var Cancer = "2000-07-17"
 var Leo = "2000-08-17"
-let Virgo = "2000-09-17"
-let Libra = "2000-10-17"
+var Virgo = "2000-09-17"
+var Libra = "2000-10-17"
 var Scorp = "2000-11-17"
 var Sagg = "2000-12-17"
 
 
 
-function newFunc(zodiac){
-let opts = {
-	mystic_dob: zodiac,
+async function newFunc(zodiac){
+var opts = {
+	mystic_dob: zodiac
 } 
-let tavelInfo =
+var travelInfo =
 
-	fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
+	await fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
 		{
 			method: 'post',
 			headers: {
@@ -67,62 +57,16 @@ let tavelInfo =
 		})
 		.then(result => result.json())
 		.then(data => {
+			console.log(data)
 			console.log(data.data["Travel Habits"])
+			return data.data["Travel Habits"]
 
 	
 		})
-
+		console.log(travelInfo)
+		return travelInfo
 
 	}
-
-	// fetch('https://corona.lmao.ninja/v2/states')
-	// .then(result => result.json())
-	// .then(data => {
-	// 	console.log(data)
-	// 	let htmlArr = data.map(obj =>{
-	// 		return `<li>${obj.state} cases: ${obj.cases} today: ${obj.todayCases}</li>`
-	// 	})
-	// 	let ul = document.querySelector('ul');
-	// 	ul.innerHTML = htmlArr.join('');
-	// })
-
-
-	// .catch((error)=>{
-	// 	console.log(error)
-	// })
-
-
-
-
-		// let travelInfo = 
-	// fetch("https://astrology-horoscope.p.rapidapi.com/zodiac_astrology/result/",
-	// 	{
-	// 		method: 'post',
-	// 		headers: {
-	// 			"content-type": "application/x-www-form-urlencoded",
-	// 			"x-rapidapi-key": "5ebc03e641msh91de1db69ede71dp13e0f0jsnb8c37847f3b1",
-	// 			"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com",
-	// 			"useQueryString": true
-	// 		},
-	// 		body: JSON.stringify(opts)
-	// 	})
-	// 	travelInfo.then((results) => {
-	// 		console.log(results.json())
-	
-	// 	})
-			
-	
-	// 	.then((data) => {
-	// 		console.log(data)
-	// 	})
-
-
-
-
-	//	})
-
-
-
 
 
 
@@ -184,6 +128,5 @@ pisces1.addEventListener('click',()=>{
 console.log(newFunc(Virgo));
 
 
-// 		})
-// }
+
 
