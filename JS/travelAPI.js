9fef7d027a452b7a267dd41308b9d486e5732fcc
Matthew <Matthewtroberts10@gmail.com>
2021-01-04 11:32:21 -0600

    added images and deleted travel clone

--- JS/travelAPI.js
+++
@@ -1,7 +0,0 @@
-
-// https://www.triposo.com/api/20201111/location.json?id=London&account=<account ID>&token=<API token>
-
-
-
-// https://www.triposo.com/api/20201111/location.json?id=London&account=<account ID>&token=<API token>
-
